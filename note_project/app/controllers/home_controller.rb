2473a7aea6a5c7d74976fa7034096094509eb672
LeeHanna <71548623+lee1nna@users.noreply.github.com>
2021-04-16 15:04:31 +0900

    note_project_crud

--- note_project/app/controllers/home_controller.rb
+++ note_project/app/controllers/home_controller.rb
@@ -7,17 +7,31 @@ class HomeController < ApplicationController
   end
 
   def create
-    @post = Post.new
-    @post.title = params[:post_title]
-    @post.content = params[:post_content]
-    @post.save
+    post = Post.new
+    post.title = params[:post_title]
+    post.content = params[:post_content]
+    post.save
 
     redirect_to "/home/index"
   end
 
   def destroy
-    @post = Post.find(params[:post_id])
-    @post.destroy
+    post = Post.find(params[:post_id])
+    post.destroy
     redirect_to "/home/index"
   end
+
+  def edit
+    @post = Post.find(params[:post_id])
+  end
+
+  def update
+    post = Post.find(params[:post_id])
+    post.title = params[:post_title]
+    post.content = params[:post_content]
+    post.save
+    
+    redirect_to "/home/index"
+  end
+  
 end
--- note_project/config/routes.rb
+++ note_project/config/routes.rb
@@ -1,9 +1,16 @@
 Rails.application.routes.draw do
   root "home#index"
+
   get 'home/index'
+
   get 'home/new'
+
   post 'home/create'
-  #
+  
   get 'home/destroy/:post_id' => 'home#destroy'
+
   get 'home/edit/:post_id' => 'home#edit'
+
+  post 'home/update/:post_id' => 'home#update'
+  
 end
