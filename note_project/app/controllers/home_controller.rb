d247d486622a941e1670c7a88bbf71d247e04da8
LeeHanna <71548623+lee1nna@users.noreply.github.com>
2021-04-21 11:04:40 +0900

    rails_form_for

--- note_project/app/controllers/home_controller.rb
+++ note_project/app/controllers/home_controller.rb
@@ -10,7 +10,7 @@ class HomeController < ApplicationController
   def create
     @post = Post.new
     @post.title = params[:post][:title]
-    @post.content = params[:post][:title]
+    @post.content = params[:post][:content]
     @post.save
 
     redirect_to "/home/index"
