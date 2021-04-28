5f413c9641aed80bdc838ebbc58c3569bedcc7af
LeeHanna <71548623+lee1nna@users.noreply.github.com>
2021-04-28 10:06:32 +0900

    change_format

--- note_project/app/controllers/home_controller.rb
+++ note_project/app/controllers/home_controller.rb
@@ -1,4 +1,6 @@
 class HomeController < ApplicationController
+  before_action :set_post, only: [:edit, :update, :destroy]
+
   def index
     @posts = Post.all
   end
@@ -8,31 +10,40 @@ class HomeController < ApplicationController
   end
 
   def create
-    @post = Post.new
-    @post.title = params[:post][:title]
-    @post.content = params[:post][:content]
-    @post.save
-
-    redirect_to posts_path
+    @post = Post.new(post_params)
+    respond_to do |format|
+      if @post.save
+        format.html { redirect_to posts_path, notice: '게시물이 성공적으로 작성되었습니다.'}
+      end
+        format.html { render :new }
+    end
   end
 
   def destroy
-    post = Post.find(params[:id])
-    post.destroy
+    @post.destroy
     redirect_to posts_path
   end
 
   def edit
-    @post = Post.find(params[:id])
   end
 
   def update
-    post = Post.find(params[:id])
-    post.title = params[:post_title]
-    post.content = params[:post_content]
-    post.save
-    
-    redirect_to posts_path
+    respond_to do |format|
+      if @post.update(post_params)
+        format.html{ redirect_to posts_path, notice: '게시물이 성공적으로 수정되었습니다.'}
+      else
+      end
+    end
   end
   
+  private
+  
+  def set_post
+    @post = Post.find(params[:id])
+  end
+
+  def post_params
+    params.require(:post).permit(:title, :content)
+  end
 end
+
